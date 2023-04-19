ea91dfde5a71f8dde91894f28f1d5055f98558f6
Kenta <102387086+Kenta30@users.noreply.github.com>
2023-04-19 10:40:57 +0200

    Update and rename __resource.lua to fxmanifest.lua

---
+++ hotpursuit/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version   'cerulean'
+lua54        'yes'
+game         'gta5'
+
+description 'Gamemode by Nynjardin.'
+
+client_scripts {
+  'hp_cl.lua',
+  'garage.lua'
+  }
+
+server_script 'hp_srv.lua'
+
+shared_script '@es_extended/imports.lua'
